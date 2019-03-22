662fe2d1d4b14113e00f1cfb393badc010c6129f
paxti <iurii.tverezovskyi@gmail.com>
2019-03-22 00:53:40 -0500

    Test

--- src/routes/api/roles.js
+++ src/routes/api/roles.js
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (_req, res, _next) => {
-  res.json({ tags: 'Hello world' });
+  res.json({ tags: 'Hello world 111' });
 });
 
 module.exports = router;
