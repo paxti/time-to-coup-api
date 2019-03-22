293f51687f730ea969ea4d7a28bd52663a4bc451
paxti <iurii.tverezovskyi@gmail.com>
2019-03-22 00:49:20 -0500

    Add pipeline for Cloud Build

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import app from './app';
 
 // eslint-disable-next-line no-console
-app.listen(3600, () => console.log('Server is listening on port 3600'));
+app.listen(8080, () => console.log('Server is listening on port 3600'));
 module.exports = app;
