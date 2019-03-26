40e085cb99ef9a3a1cbd1d2b58a72f548aaec6e4
paxti <iurii.tverezovskyi@gmail.com>
2019-03-25 22:32:25 -0500

    Change initial message to reflect new default port

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import app from './app';
 
 // eslint-disable-next-line no-console
-app.listen(8080, () => console.log('Server is listening on port 3600'));
+app.listen(8080, () => console.log('Server is listening on port 8080'));
 module.exports = app;
