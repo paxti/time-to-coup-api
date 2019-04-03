0c6ddfa95daf55e53439f3eeec22c4245fa31416
paxti <iurii.tverezovskyi@gmail.com>
2019-04-02 23:34:04 -0500

    Use env var for a port

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
+import {} from 'dotenv/config';
 import app from './app';
 
 // eslint-disable-next-line no-console
-app.listen(8080, () => console.log('Server is listening on port 8080'));
+app.listen(process.env.PORT, () =>
+  console.log(`Server is listening on port ${process.env.PORT}`)
+);
 module.exports = app;
